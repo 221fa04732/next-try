66912f9a8b801afca1d5183c0b26fed30cea8a7c
221fa04732 <221fa04732@gmail.com>
2025-06-09 01:20:34 +0530

    modify routing logic

--- component/header.tsx
+++ component/header.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
 
+const rout = [{
+    "path" : "/hii",
+    "name" : "first route"
+},{
+    "path" : "/hii2",
+    "name" : "second route"
+}]
 export default function Header() {
   return (<div>
-    <Link href={"/hii"} >hii</Link>
-    <Link href={"/hii2"} >hii2</Link>
+    {rout.map((item)=>(
+        <Link href={item.path}>{item.name}</Link>
+    ))}
   </div>
   );
 }
